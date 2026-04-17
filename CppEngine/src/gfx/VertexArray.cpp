e19f7b63c24f909e8fe4f746c0278f22e08c5026
axedd <frederikmdjayz@outlook.com>
2026-04-17 17:53:39 +0200

    add: vertexarray implemenation

---
+++ CppEngine/src/gfx/VertexArray.cpp
@@ -0,0 +1,26 @@
+#include "VertexArray.h"
+#include <glad/glad.h>
+
+
+VertexArray::VertexArray() {
+    glGenVertexArrays(1, &m_RendererID);
+}
+
+VertexArray::~VertexArray() {
+    glDeleteVertexArrays(1, &m_RendererID);
+}
+
+void VertexArray::bind() const {
+    glBindVertexArray(m_RendererID);
+}
+
+void VertexArray::unbind() const {
+    glBindVertexArray(0);
+}
+
+void VertexArray::addBuffer(const VertexBuffer& vb, unsigned int index, int count, int stride, int offset) {
+    bind();
+    vb.bind();
+    glEnableVertexAttribArray(index);
+    glVertexAttribPointer(index, count, GL_FLOAT, GL_FALSE, stride, (const void*)offset);
+}
